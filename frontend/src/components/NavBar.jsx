d5b8616346c86c021d716d8efcef793c1e5db9ff
manish-107 <creativecrew2676@gmail.com>
2024-02-05 21:23:43 +0530

    frontend starts

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <h1 className="text-3xl font-bold underline p-20">
+            Hello world!
+        </h1>
+    )
+}
+
+export default NavBar
